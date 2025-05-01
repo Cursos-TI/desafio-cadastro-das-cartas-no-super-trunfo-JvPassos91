b00c25f10d62df08c58f046500bf73374efb7ac7
JvPassos91 <jvpsss1991@gmail.com>
2025-04-30 23:34:27 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    int carta;
+    char Estado[15]; 
     int populacao;
     float area;
     float pib;
     int pontosturisticos;
+
+printf("Selecione a sua carta:\n");
+scanf("%d", &carta);
+
+printf("Digite o estado que sua carta representa:\n");
+scanf("%s", &estado);
 
 printf("Digite a População:\n");
 scanf ("%d", &populacao);
@@ -18,8 +26,14 @@ scanf ("%d", &pib);
 printf ("Digite a quantidade de Pontos Turisticos:\n");
 scanf ("%f", &pontosturisticos);
 
-printf("População: %d - Área: %f\n", populacao , area);
-printf("PIB: %d - Quantidade de Pontos Turisticos: %d\n", pib, pontosturisticos);
+printf("População: %d\n", populacao);
+    
+printf("Área: %d\n", area);
+
+printf("PIB: %d\n", pib);
+
+printf("Pontos Turísticos:%d/n", pontosturisticos);
+    
 
 
     return 0;
